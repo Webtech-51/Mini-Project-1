d73e9c4cb8dcaf53f9dce01d530a5a157f7fae94
Webtech-51 <116025002+Webtech-51@users.noreply.github.com>
2022-11-29 10:31:27 +0530

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBGkjtonAJ4wThT4Rm1jgiN10O1St2lAns",
+    authDomain: "mini1-5f886.firebaseapp.com",
+    projectId: "mini1-5f886",
+    storageBucket: "mini1-5f886.appspot.com",
+    messagingSenderId: "675161695094",
+    appId: "1:675161695094:web:5bea5094c58ed9b2cc5057"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
